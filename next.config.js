e3f3dd1e10e94e5c0f7d31c4d51d15b7acc403b0
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-06-14 21:43:36 +0900

    static

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    reactStrictMode:true,
+    swcMinify: true,
+    images: {
+        domains: ['images.dog.ceo'],
+    },
+}
 
 module.exports = nextConfig
